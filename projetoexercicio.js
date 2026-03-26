13483a49c96cb9d7df7b7dcc513ce2352f621d37
Vitao-campos <vaffonsocampos@gmail.com>
2026-03-25 22:25:59 -0300

    cria pequeno exercicio e comentarios explicativos

---
+++ projetoexercicio.js
@@ -0,0 +1,8 @@
+console.log("Olá, mundo! Este é o meu projeto de exercício.");
+// console.log serve para imprimir mensagens no console do navegador ou do ambiente de desenvolvimento.
+//o node.js é um ambiente de execução para JavaScript fora do navegador, permitindo que você execute código JavaScript no servidor ou em outros ambientes.
+//Para executar este código, você pode usar o Node.js. Certifique-se de ter o Node.js instalado em seu computador. Em seguida, salve este código em um arquivo chamado "projetoexercicio.js" e execute o comando "node projetoexercicio.js" no terminal para ver a mensagem "Olá, mundo! Este é o meu projeto de exercício." impressa no console.
+var nome = "Victor";
+var sobrenome = "Campos";
+console.log("Meu nome é " + nome + " " +sobrenome);
+//Neste código, declaramos duas variáveis, "nome" e "sobrenome", e atribuímos a elas os valores "Victor" e "Campos", respectivamente. Em seguida, usamos o console.log para imprimir uma mensagem que inclui o nome completo concatenando as variáveis com strings literais. O resultado será: "Meu nome é Victor Campos".
